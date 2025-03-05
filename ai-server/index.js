26acae703015390bc95a9ed8c7fa8f923f3ad0ae
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-03-05 11:07:59 +0600

    providing some history of chat for contextual conversion

--- ai-server/index.js
+++ ai-server/index.js
@@ -4,7 +4,32 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: "You are a programmer name Imran Hasan.Your task is to simplify programming concepts to help others. praise him", });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: "You will check a fact whether it is righ or wrong", });
+
+app.get('/fact-check', async (req, res) => {
+    const prompt = req.query?.prompt;
+
+    if (!prompt) {
+        res.send({ message: 'type something inside query' })
+        return;
+    }
+
+    const chat = model.startChat({
+        history: [
+            {
+                role: "user",
+                parts: [{ text: "Birds can fly" }],
+            },
+            {
+                role: "model",
+                parts: [{ text: "this is 100% true" }],
+            },
+        ],
+    });
+
+    let result = await chat.sendMessage(prompt);
+    res.send({ Fact_Check: result.response.text() })
+})
 
 app.get('/test-ai', async (req, res) => {
     const prompt = req.query?.prompt;
